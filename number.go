00fad4be752bf4c27dfda35764ff2614fa8b288e
dreamans <dreamans@163.com>
2018-12-21 16:12:42 +0800

    fix number = 0 cannot valided bug

--- number.go
+++ number.go
@@ -52,10 +52,6 @@ func numMinMaxHandler(value reflect.Value, params []string, op byte) bool {
         val = float64(value.Float())
     }
 
-    if val == 0 {
-        return true
-    }
-
     switch op {
     case 'l':
         if val < pa1 {
